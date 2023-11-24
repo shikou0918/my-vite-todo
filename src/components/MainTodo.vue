ce9682ba5070ee5c64f3907d1ca8a418dfd28eca
shikou0918 <shikou.0918@gmail.com>
2023-11-24 17:27:33 +0900

    TODO一覧表示

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -4,6 +4,9 @@ import { ref } from 'vue';
 const todoRef = ref('');
 const todoListRef = ref([]);
 
+const ls = localStorage.todoList
+todoListRef.value = ls ? JSON.parse(ls) : [];
+
 const addTodo = () => {
   const id = new Date().getTime();
   todoListRef.value.push({id: id, task: todoRef.value });
@@ -21,6 +24,17 @@ const addTodo = () => {
       v-model="todoRef"
       placeholder="+ TODOを入力">
     <button class="btn" @click="addTodo">追加</button>
+  </div>
+  <div class="box_list">
+    <div class="todo_list" v-for="todo in todoListRef" :key="todo.id">
+      <div class="todo">
+        <input type="checkbox" class="check" /><label>{{ todo.task }}</label>
+      </div>
+      <div class="btns">
+        <button class="btn green">編</button>
+        <button class="btn pink">削</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -46,5 +60,42 @@ const addTodo = () => {
   text-align: center;
   font-size: 14px;
 }
+.box_list {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+}
 
+.todo_list {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.todo {
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  padding: 12px;
+  width: 300px;
+}
+
+.check {
+  border: 1px solid red;
+  transform: scale(1.6);
+  margin: 0 16px 2px 6px;
+}
+
+.btns {
+  display: flex;
+  gap: 4px;
+}
+
+.green {
+  background-color: #00c853;
+}
+
+.pink {
+  background-color: #ff4081;
+}
 </style>
