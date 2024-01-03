cfb30e80d6cc534a6c5fd106c7bb8b782f82b672
shikou0918 <shikou.0918@gmail.com>
2024-01-03 19:01:41 +0900

    コーディング練習

--- src/index.js
+++ src/index.js
@@ -25,8 +25,8 @@ const routes = [
   },
   {
     path: '/blog/:id',
-    name: 'BlogId',
-    component: () => import('/src/pages/Blog.vue'),
+    name: 'BlogDtl',
+    component: () => import('/src/pages/BlogDtl.vue'),
   },
   {
     path: '/blog',
---
+++ src/pages/BlogDtl.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const id = route.params.id;
+
+const dtl = ref({});
+const fetchData = async () => {
+  const response = await fetch ('https://jsonplaceholder.typicode.com/posts/' + id);
+  dtl.value = await response.json();
+};
+fetchData();
+
+</script>
+
+<template>
+  <div>
+    <h1 class="title">{{ dtl.title }}</h1>
+    <h1 class="dtl">{{ dtl.body }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  margin: 12px;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+}
+
+.dtl {
+  line-height: 1.5;
+}
+</style>
